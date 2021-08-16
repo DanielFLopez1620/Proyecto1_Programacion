1fe804c27f583b5a8c9e62182878f5796116c7be
dasammh <dddemmhr@gmail.com>
2021-08-16 09:55:31 -0500

    cambio_david

--- proyecto-1.cpp
+++ proyecto-1.cpp
@@ -6,6 +6,6 @@ int main()
     int var;
     cout<<"Probando Hola Mundo, con Live Share"<<endl;
     cout<<"ESte es otro cambio desde local"<<endl;
-    cout<<"que mas "<<endl;
+    cout<<"que mas "<<endl; //hola
     return 0; //hola  
 }
