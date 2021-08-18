b189f1e080091f82cd385a8526326c03d26b2dba
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2021-08-18 11:24:00 -0500

    menus_principales

--- proyecto-1.cpp
+++ proyecto-1.cpp
@@ -6,7 +6,9 @@
 #include<time.h>  //librería de manejo de tiempo y fechas
 using namespace std;
 //--PROTOTIPOS DE FUNCIONES:
-
+void menu_admin();
+void menu_client();
+void menu_consul();
 //--DESARROLLO DEL MAIN:
 int main()
 {
@@ -20,3 +22,43 @@ int main()
     return 0;   
 }
 //--DEFINICIÓN DE FUNCIONES
+void menu_admin()
+{
+    cout<<"*********************************"<<endl;
+    cout<<"*   OPCIONES DE ADMINISTRADOR   *"<<endl;
+    cout<<"*********************************"<<endl;
+    cout<<"*   1) Habilitar registros      *"<<endl;
+    cout<<"*   2) Desbloquear usuarios     *"<<endl;
+    cout<<"*   3) Administrar categorías   *"<<endl;
+    cout<<"*   4) Administrar productos    *"<<endl;
+    cout<<"*   5) Despacha compras         *"<<endl;
+    cout<<"*   6) Cerrar sesión            *"<<endl;
+    cout<<"*********************************"<<endl;
+    return;
+}
+void menu_client()
+{
+    cout<<"*********************************"<<endl;
+    cout<<"*      OPCIONES DE CLIENTE      *"<<endl;
+    cout<<"*********************************"<<endl;
+    cout<<"*   1) Realizar una compra      *"<<endl;
+    cout<<"*   2) Cancelar una compra      *"<<endl;
+    cout<<"*   3) Valor total de compra    *"<<endl;
+    cout<<"*   4) Cantidad de productos    *"<<endl;
+    cout<<"*   5) Cerrar Sesión            *"<<endl;
+    cout<<"*********************************"<<endl;
+    return;
+}
+void menu_consul()
+{
+    cout<<"*********************************"<<endl;
+    cout<<"*     OPCIONES DE CONSULTOR     *"<<endl;
+    cout<<"*********************************"<<endl;
+    cout<<"*   1) Listado de productos     *"<<endl;
+    cout<<"*   2) Listado de clientes      *"<<endl;
+    cout<<"*   3) Total de ventas          *"<<endl;
+    cout<<"*   4) Producto más vendido     *"<<endl;
+    cout<<"*   5) Cerrar sesión            *"<<endl;
+    cout<<"*********************************"<<endl;
+    return;
+}
