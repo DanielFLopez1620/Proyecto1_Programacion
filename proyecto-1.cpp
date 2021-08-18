ac7892e38618bd9b0047634a0749f3fcb0ac9846
DanielFLopez1620 <dfelipe.lopez@gmail.com>
2021-08-18 11:09:27 -0500

    configuración_base

--- proyecto-1.cpp
+++ proyecto-1.cpp
@@ -1,11 +1,22 @@
 //---LIBRERÍAS:
 #include<iostream>
+#include<locale.h>   //librería de configuración regional
+#include<fstream>
+#include<string.h>  
+#include<time.h>  //librería de manejo de tiempo y fechas
 using namespace std;
+//--PROTOTIPOS DE FUNCIONES:
+
+//--DESARROLLO DEL MAIN:
 int main()
 {
     int var;
-    cout<<"Probando Hola Mundo, con Live Share"<<endl;
-    cout<<"ESte es otro cambio desde local"<<endl;
-    cout<<"que mas "<<endl; //hola
-    return 0; //hola  
+    setlocale(LC_ALL,"");  //configuración de región
+    time_t rawtime;    //variable de tipo tiempo
+    struct tm * timeinfo;  //estructura de tiempo que abarca desde segundo a mes--> tm_(type)
+    time(&rawtime);    //función para obtener el tiempo actual
+    timeinfo = localtime(&rawtime);  //cambiar el formato a la zona configurada
+    //para imprimeir el tiempo usar el comando asctime(timeinfo);
+    return 0;   
 }
+//--DEFINICIÓN DE FUNCIONES
